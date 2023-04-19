00b7bc8cdbdb82f5bfdcde03eaee3ca878d005b2
hodorsec <hodor@hodorsec.com>
2023-04-19 21:41:35 +0200

    Added repo's as deb woed due to libc6 deps

--- Vagrant/Debian_Kali-Repo_Virtualbox/modules/kali_apt/manifests/init.pp
+++ Vagrant/Debian_Kali-Repo_Virtualbox/modules/kali_apt/manifests/init.pp
@@ -1,10 +1,18 @@
 class kali_apt {
+  exec { 'echo_kali':
+    command => "echo 'deb http://http.kali.org/kali kali-rolling main non-free non-free-firmware contrib' > /etc/apt/sources.list",
+    path    => ['/usr/bin', '/usr/sbin',],
+  }
+  exec { 'echo_kali-src':
+    command => "echo 'deb-src http://http.kali.org/kali kali-rolling main non-free non-free-firmware contrib' >> /etc/apt/sources.list",
+    path    => ['/usr/bin', '/usr/sbin',],
+  }
   exec { 'echo_deb':
-    command => "echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list",
+    command => "echo 'deb http://deb.debian.org main non-free non-free-firmware contrib' >> /etc/apt/sources.list",
     path    => ['/usr/bin', '/usr/sbin',],
   }
   exec { 'echo_deb-src':
-    command => "echo 'deb-src http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list",
+    command => "echo 'deb-src http://deb.debian.org main non-free non-free-firmware contrib' >> /etc/apt/sources.list",
     path    => ['/usr/bin', '/usr/sbin',],
   }
   exec { 'wget_asc':
